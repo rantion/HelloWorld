adc497dbd0d5681c4e1cd22c5f970e1ab35371ad
rantion <Rachel@Rachels-MacBook-Pro.local>
2013-10-11 07:49:33 -0600

    Hello World!

---
+++ HelloWorld/src/HelloWorldMain.java
@@ -0,0 +1,17 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Rachel
+ * Date: 10/9/13
+ * Time: 8:47 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorldMain {
+
+    public static void main (String[]args){
+          printStuff();
+    }
+
+    public static void printStuff()    {
+        System.out.println("Hello World");
+    }
+}
